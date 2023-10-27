d6b2a95ef5926db41c8b7cbac5011db37b38000c
groza29 <groza.emanuel4@gmail.com>
2023-10-27 19:15:11 +0300

    Login and Register works

---
+++ client/src/Header.jsx
@@ -0,0 +1,80 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
+export default function Header() {
+  const { user } = useContext(UserContext);
+  return (
+    <header className=" flex justify-between">
+      <a href="" className="flex items-center gap-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-8 h-8 -rotate-90"
+        >
+          <path d="M3.105 2.289a.75.75 0 00-.826.95l1.414 4.925A1.5 1.5 0 005.135 9.25h6.115a.75.75 0 010 1.5H5.135a1.5 1.5 0 00-1.442 1.086l-1.414 4.926a.75.75 0 00.826.95 28.896 28.896 0 0015.293-7.154.75.75 0 000-1.115A28.897 28.897 0 003.105 2.289z" />
+        </svg>
+        <span className="font-bold text-xl">Reservify</span>
+      </a>
+      <div className="flex gap-3 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gry-300">
+        <div>Anywhere</div>
+        <div className="border-l border-gray-300"></div>
+        <div>Any week</div>
+        <div className="border-l border-gray-300"></div>
+        <div>Add guests</div>
+        <button className="bg-primary text-white p-1.5 rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </div>
+      <Link
+        to={user ? "/account" : "/login"}
+        className="flex items-center gap-3 border border-gray-300 rounded-full py-2 px-4 shadow-md"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+        <div className="bg-primary text-white rounded-full p-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+            />
+          </svg>
+        </div>
+        {!!user && <div>{user.name}</div>}
+      </Link>
+    </header>
+  );
+}
